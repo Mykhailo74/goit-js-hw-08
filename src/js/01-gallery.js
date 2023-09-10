63abbff1d4dd2ec3dd393710bc1385e210f74c68
Mykhailo74 <130218368+Mykhailo74@users.noreply.github.com>
2023-09-10 20:49:43 +0300

    Task-1

---
+++ src/js/01-gallery.js
@@ -0,0 +1,31 @@
+import SimpleLightbox from 'simplelightbox';
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+// Add imports above this line
+
+import { galleryItems } from './gallery-items';
+
+// Change code below this line
+
+const galleryElements = document.querySelector('.gallery');
+
+
+const createGalleryMarkup = galleryItems => {
+    return galleryItems
+    .map(({ preview, original, description }) => {
+        return `
+            <a class="gallery__item" href=${original}>
+                <img class="gallery__image" src=${preview} alt=${description} />
+            </a>`;
+    })
+    .join('');
+};
+
+galleryElements.insertAdjacentHTML('beforeend', createGalleryMarkup(galleryItems));
+
+const lightbox = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+});
+
+console.log(galleryItems);
